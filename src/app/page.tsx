0628636da2a89a4531869fadd463c7a812047556
Abiel Rodriguez <abiel.rodriguez@volenday.com>
2024-06-04 21:37:20 +0100

    chore: Update landing page content and styles

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,12 @@
-import Image from 'next/image';
-
-export default function Home() {
+export default function Home(): JSX.Element {
   return (
-    <main>
-      <h1>New portfolio</h1>
+    <main className="flex flex-col">
+      <h2 className="font-medium text-[rgb(59, 61, 65)] text-[20px] opacity-40">Hello there!</h2>
+      <span className="mr-10 break-words text-start tracking-tight  mt-4 text-xl md:text-4xl opacity-50 font-semibold">
+        Ethan is a designer crafting interfaces and interactions that are useful and feel natural.
+        Previously, he designed human interfaces at Apple and has worked for Marqeta and GoodNotes.
+        He&apos;s currently a designer at Berkeleytime.
+      </span>
     </main>
   );
 }
